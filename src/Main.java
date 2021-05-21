24c9a86a2d6ff8e70bab94bada72301c6ca88104
LT-6FGD373\Hannes <h.roever@zertificon.com>
2021-05-21 12:58:05 +0200

    change: flag to load accounts

--- src/Main.java
+++ src/Main.java
@@ -10,19 +10,23 @@ public class Main
     
     public static void main(String[] args) throws IOException, ClassNotFoundException 
     {  
-        boolean createAndSaveDummyAccounts = true;        
+        boolean createAndSaveDummyAccounts = true;     
+        boolean loadAccounts = true;   
         var secureAccounts = new SecureAccountsController();
 
         if (createAndSaveDummyAccounts)
         {
             bank = new Bank();
             var accountFabric = new AccountFabric(bank);
-            accountFabric.createRandomAccounts(20);        
+            accountFabric.createRandomAccounts(3);        
             bank.listAllCustomers();
             secureAccounts.saveAccountsToFile(bank);
         }        
 
-        var bankLoaded = secureAccounts.loadAccountsTFromFile();
-        bankLoaded.listAllCustomers();
+        if (loadAccounts)
+        {
+            var bankLoaded = secureAccounts.loadAccountsTFromFile();
+            bankLoaded.listAllCustomers();
+        }
     }  
 }
